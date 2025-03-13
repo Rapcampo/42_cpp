25844a2b2a7ba34cf16c038214f8b59e2ad2a27d
Raphael Vieira <142260510+Rapcampo@users.noreply.github.com>
2025-03-13 15:50:42 +0000

    Add files via upload

--- cpp00/ex01/includes/main.hpp
+++ cpp00/ex01/includes/main.hpp
@@ -6,7 +6,7 @@
 /*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 22:18:20 by rapcampo          #+#    #+#             */
-/*   Updated: 2025/03/08 14:55:30 by rapcampo         ###   ########.fr       */
+/*   Updated: 2025/03/13 14:35:40 by rapcampo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@
 # define CYN "\e[1;36m"
 # define WHT "\e[1;37m"
 # define CLR "\e[0;39m"
+
+//error input
 # define IN_ERR "\nEOF input is not valid, good try tho!"
 
 void	DisplaySearchPrompt(PhoneBook *bookake);
