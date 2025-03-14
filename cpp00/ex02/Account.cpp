2aa68b9bdef67e9fa4114d1da92b3c48873aa957
Raphael Campos Vieira <rapcampo@c2r4s5.42porto.com>
2025-03-14 19:31:57 +0000

    timestamp thingy done

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -24,23 +24,44 @@ int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
-void Account::_displayTimestamp(void);
+void Account::_displayTimestamp(void){
+	std::time_t t = std::time(NULL);
+	std::tm *tm = std::localtime(&t);
+	std::cout << "[" << std::setfill('0') << std::setw(4) << (tm->tm_year + 1900)
+		<< std::setfill('0') << std::setw(2) << (tm->tm_mon + 1) << std::setfill('0')
+		<< std::setw(2) << tm->tm_mday << "_"
+		<< std::setfill('0') << std::setw(2) << tm->tm_hour <<std::setfill('0')
+		<< std::setw(2) << tm->tm_sec << std::setfill('0') << std::setw(2)
+		<< tm->tm_sec << "] ";
+}
 
 Account::Account(void){
 	return;
 }
 
+//needs print time, index, amount from initial dep and "created"
+Account::Account(int initial_deposit);
+
+//needs to print time, index number and amount in account, followed by status "closed"
 Account::~Account(void){
+	Account::_displayTimestamp();
+	std::cout << "index:" << Account::_accountIndex << ";" << "amount:"
+		<< Account::_amount << ";" << "closed";
 	return;
 }
 
+//prints index, previous amount, amound deposited, current and global deposit
 void Account::makeDeposit(int deposit);
 
+//should return boolean if amount in account is enough for withdrawal??
+//prints index of previous amount, amount w, current amount and global nb of with
 bool Account::makeWithdrawal(int withdrawal);
 
-Account::Account(int initial_deposit);
-
-void Account::displayAccountsInfos(void);
+//prints time, index, current balance, nb deposits, nb with;
+void Account::displayAccountsInfos(void){
+	Account::_displayTimestamp();
+	for (i < Account::getNbAccounts();
+}
 
 int	Account::getNbAccounts(void){
 	return (Account::_nbAccounts);
@@ -62,7 +83,8 @@ int	Account::checkAmount(void) const {
 	return (this->_amount);
 }
 
-//note: need to figure out from logs still
+//note: need to figure out from logs still, likely used by display accounts info
+//likely prints both deposits and with
 void Account::displayStatus(void) const {
 	return;
 }
