c3615609d569bedea0684b43d9486e7f63570f1d
Raphael_Linux <raphael.campospt@gmail.com>
2025-03-15 00:47:13 +0000

    diff finds no difference. everything seems to work fine

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -33,6 +33,7 @@ void Account::_displayTimestamp(void){
 		<< std::setfill('0') << std::setw(2) << tm->tm_hour <<std::setfill('0')
 		<< std::setw(2) << tm->tm_sec << std::setfill('0') << std::setw(2)
 		<< tm->tm_sec << "] ";
+	return;
 }
 
 Account::Account(void){
@@ -40,27 +41,70 @@ Account::Account(void){
 }
 
 //needs print time, index, amount from initial dep and "created"
-Account::Account(int initial_deposit);
+Account::Account(int initial_deposit){
+	Account::_displayTimestamp();
+	Account::_accountIndex = getNbAccounts();
+	Account::_nbAccounts++;
+	Account::_amount = initial_deposit;
+	Account::_totalAmount += initial_deposit;
+	std::cout << "index:" << Account::_accountIndex	<< ";" 
+		<< "amount:" << Account::_amount << ";" << "created" << std::endl;
+	return;
+}
 
 //needs to print time, index number and amount in account, followed by status "closed"
 Account::~Account(void){
 	Account::_displayTimestamp();
 	std::cout << "index:" << Account::_accountIndex << ";" << "amount:"
-		<< Account::_amount << ";" << "closed";
+		<< Account::_amount << ";" << "closed" << std::endl;
 	return;
 }
 
 //prints index, previous amount, amound deposited, current and global deposit
-void Account::makeDeposit(int deposit);
+void Account::makeDeposit(int deposit){
+	int p_amount = Account::_amount;
+	Account::_amount += deposit;
+	Account::_nbDeposits++;
+	Account::_totalNbDeposits++;
+	Account::_totalAmount += deposit;
+	Account::_displayTimestamp();
+	std::cout << "index:" << Account::_accountIndex << ";"
+		<< "p_amount:" << p_amount << ";" << "deposit:" << deposit << ";"
+		<< "amount:" << Account::_amount << ";" << "nb_deposits:"
+		<< Account::_nbDeposits << std::endl;
+	return;
+}
 
 //should return boolean if amount in account is enough for withdrawal??
 //prints index of previous amount, amount w, current amount and global nb of with
-bool Account::makeWithdrawal(int withdrawal);
+bool Account::makeWithdrawal(int withdrawal){
+	Account::_displayTimestamp();
+	int p_amount = Account::_amount;
+	if ((Account::_amount - withdrawal) < 0){
+		std::cout << "index:" << Account::_accountIndex << ";" << "p_amount:"
+			<< p_amount << ";" << "withdrawal:refused" << std::endl;
+		return (false);
+	}
+	else {
+		Account::_amount -= withdrawal;
+		Account::_nbWithdrawals++;
+		Account::_totalNbWithdrawals++;
+		Account::_totalAmount -= withdrawal;
+		std::cout << "index:" << Account::_accountIndex << ";" << "p_amount:"
+			<< p_amount << ";" << "withdrawal:" << withdrawal << ";"
+			<< "amount:" << Account::_amount << ";" << "nb_withdrawals:"
+			<< Account::_nbWithdrawals << std::endl;
+	}
+	return (true);
+}
 
 //prints time, index, current balance, nb deposits, nb with;
 void Account::displayAccountsInfos(void){
 	Account::_displayTimestamp();
-	for (i < Account::getNbAccounts();
+	std::cout << "accounts:" << Account::_nbAccounts << ";" << "total:"
+		<< Account::_totalAmount << ";" << "deposits:" << Account::_totalNbDeposits
+		<< ";" << "withdrawals:" << Account::_totalNbWithdrawals << std::endl;
+	return;
 }
 
 int	Account::getNbAccounts(void){
@@ -86,5 +130,10 @@ int	Account::checkAmount(void) const {
 //note: need to figure out from logs still, likely used by display accounts info
 //likely prints both deposits and with
 void Account::displayStatus(void) const {
+	Account::_displayTimestamp();
+	std::cout << "index:" << Account::_accountIndex << ";"
+		<< "amount:" << Account::_amount << ";"
+		<< "deposits:" << Account::_nbDeposits << ";"
+		<< "withdrawals:" << Account::_nbWithdrawals << std::endl;
 	return;
 }
