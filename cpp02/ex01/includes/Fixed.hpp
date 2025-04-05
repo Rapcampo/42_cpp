b926a6c3ced596fae5aaca8a9e1bd25acf656e3d
Raphael_Linux <raphael.campospt@gmail.com>
2025-04-05 15:16:28 +0100

    copy from previous exercise

---
+++ cpp02/ex01/includes/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 20:19:04 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/04/03 20:27:18 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# include <iostream>
+
+class Fixed{
+	private:
+		int	_fpnv;
+		static const int _fracb = 8;
+
+	public:
+		Fixed(); //default constructor
+		Fixed(const Fixed &cp); //copy constructor
+		~Fixed(); //default destructor
+		Fixed &operator = (const Fixed &src); //Assigment Operator
+		int	getRawBits(void) const;
+		void setRawBits(int const raw);
+};
---
+++ cpp02/ex01/src/Fixed.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 20:24:36 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/04/03 20:36:07 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+Fixed::Fixed(void) : _fpnv(0){
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy; //Operator overlaod
+}
+
+Fixed::~Fixed(void){std::cout << "Destructor called" << std::endl;}
+
+//Operator 
+Fixed &Fixed::operator =(const Fixed& src){
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &src){
+		this->_fpnv = src.getRawBits();
+	}
+	return (*this);
+}
+
+int Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return(this->_fpnv);
+}
+
+void Fixed::setRawBits(int const raw) {this->_fpnv = raw;}
