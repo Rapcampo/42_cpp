48e954d1656ff6778c117b5da680e3d77fd7d12d
Rapcampo <raphael.campospt@gmail.com>
2025-04-16 12:36:28 +0100

    cpy of ex00 to all others as base

---
+++ cpp03/ex02/ex01/includes/ClapTrap.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 19:21:48 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/04/15 19:36:24 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# include <iostream>
+
+class ClapTrap{
+	private:
+		std::string _name;
+		unsigned int _hp;
+		unsigned int _energy;
+		unsigned int _attack;
+
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap();
+		ClapTrap(const ClapTrap &src);
+		ClapTrap &operator=(const ClapTrap &src);
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
---
+++ cpp03/ex03/ex01/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 20:46:17 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/04/15 21:34:16 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ClapTrap.hpp"
+#include <iostream>
+
+int main(void){
+	ClapTrap ct1("jorginho");
+	ClapTrap ct2j(ct1);
+	ct1.beRepaired(1);
+	ct1.takeDamage(2);
+	ct1.beRepaired(1);
+	ct1.beRepaired(1);
+	for (int i = 10; i > 0; i--)
+		ct1.attack("Leonildo");
+	ct1.takeDamage(10);
+}
