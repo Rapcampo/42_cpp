585da10090a4b3156dd55a5fae77ef9b430468d9
Rapcampo <raphael.campospt@gmail.com>
2025-04-17 15:51:03 +0100

    this should work now

---
+++ cpp03/ex02/src/FragTrap.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 15:49:38 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/04/17 15:50:44 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/FragTrap.hpp"
+
+
+FragTrap::FragTrap(const std::string name) : ClapTrap(name, 100, 50, 20){
+	std::cout << "\e[1;35mScavTrap "<< this->getName() << " has been created!\e[0m" << std::endl;
+}
+
+FragTrap::~FragTrap(){
+	std::cout << "\e[1;36mScavTrap " << this->getName() << " has been destroyed!\e[0m" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &src) : ClapTrap(src){
+	std::cout << "FragTrap copy constructor called" << std::endl;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &src){
+	ClapTrap::operator=(src);
+	std::cout << "FragTrap Operator overload called!" << std::endl;
+	return *this;
+}
+
+void FragTrap::attack(const std::string &target){
+	if (this->getEnergy() > 0 && this->getHealth() > 0){
+		std::cout << "FragTrap " << this->getName() << " attacks " << target
+		<< ", causing " << this->getAttack() << " points of damage!" << std::endl;
+		this->setEnergy(-1);
+	}
+	else if (this->getEnergy() <= 0){
+		std::cout << "FragTrap " << this->getName() << " has no energy left!" << std::endl;
+	}
+	else 
+		std::cout << "FragTrap " << this->getName() << " has no Health left!" << std::endl;
+}
+
+void FragTrap::highFivesGuys(){
+	std::cout << "FragTrap is requesting high fives." << std::endl;
+}
