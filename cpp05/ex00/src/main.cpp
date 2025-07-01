f89dd95fed7d66ec43659d7673bf8dc1dca304da
Rapcampo <raphael.campospt@gmail.com>
2025-07-01 23:42:35 +0100

    fixed logic in tests

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -15,7 +15,7 @@
 
 static void	testSuite(){
 
-	std::cout << YLW BLN"Test 1: incrementing too much\n" RST << std::endl;
+	std::cout << YLW "\nTest 1: incrementing too much\n" RST << std::endl;
 	try{
 		Bureaucrat highrank("BigBoss", 1);
 		std::cout << highrank << std::endl;
@@ -24,7 +24,7 @@ static void	testSuite(){
 		std::cout << e.what() << std::endl;
 	}
 
-	std::cout << BLU BLN"Test 2: decrementing too much\n" RST << std::endl;
+	std::cout << BLU "\nTest 2: decrementing too much\n" RST << std::endl;
 	try{
 		Bureaucrat lowrank("CannonFodder", 150);
 		std::cout << lowrank << std::endl;
@@ -33,21 +33,21 @@ static void	testSuite(){
 		std::cout << e.what() << std::endl;
 	}
 
-	std::cout << YLW BLN"Test 3: constructing grade too high\n" RST << std::endl;
+	std::cout << YLW "\nTest 3: constructing grade too high\n" RST << std::endl;
 	try{
 		Bureaucrat shareholder("shareholder", 0);
 	} catch(std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
 
-	std::cout << BLU BLN"Test 4: constructing grade too low\n" RST << std::endl;
+	std::cout << BLU "\nTest 4: constructing grade too low\n" RST << std::endl;
 	try{
 		Bureaucrat commoner("commoner", 151);
 	} catch(std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
 
-	std::cout << GRN BLN"Test 5: valid use cases\n" RST << std::endl;
+	std::cout << GRN "\nTest 5: valid use cases\n" RST << std::endl;
 	try{
 		Bureaucrat goodboy("Hardworker", 75);
 		std::cout << goodboy << std::endl;
@@ -56,10 +56,10 @@ static void	testSuite(){
 		goodboy.incGrade();
 		goodboy.incGrade();
 		std::cout << goodboy << std::endl;
-		while (goodboy.getGrade() <= MIN_GRADE)
+		while (goodboy.getGrade() < MIN_GRADE)
 			goodboy.decGrade();
 		std::cout << goodboy << std::endl;
-		while (goodboy.getGrade() >= MAX_GRADE)
+		while (goodboy.getGrade() > MAX_GRADE)
 			goodboy.incGrade();
 		std::cout << goodboy << std::endl;
 	} catch(std::exception &e){
