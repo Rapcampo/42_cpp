ef1224e98e6879a6717cf991e09a69a52032b1d3
Raphael Campos Vieira <rapcampo@c2r5s11.42porto.com>
2025-07-01 18:18:55 +0100

    started doing cpp05, already hating it

---
+++ cpp05/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 17:56:25 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/07/01 17:59:54 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <stdexcept>
+
+#define MAX_GRADE 1
+#define MIN_GRADE 150
+
+class Bureaucrat{
+	private:
+		const std::string	_name;
+		unsigned int		_grade;
+	
+	public:
+		Bureaucrat();
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &src);
+		Bureaucrat &operator=(const Bureaucrat &src);
+		Bureaucrat(const std::string name, int grade);
+		const std::string &getName() const;
+		unsigned int getGrade() const;
+		void incGrade();
+		void decGrade();
+		class GradeTooHighExeception : public std::runtime_error{
+			public:
+
+		};
+		class GradeTooLowException : public std::runtime_error{
+			public:
+
+		};
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj);
