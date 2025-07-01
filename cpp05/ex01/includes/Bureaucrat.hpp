59b74344c7bc2903d14d7484a86c84eafbe00f2f
Rapcampo <raphael.campospt@gmail.com>
2025-07-01 23:56:09 +0100

    saving changes to ex01

---
+++ cpp05/ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 17:56:25 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/07/01 17:59:54 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <exception>
+
+#define MAX_GRADE 1
+#define MIN_GRADE 150
+
+//colour prints
+
+# define RST "\e[0m"
+# define ULI "\e[4m"
+# define BLN "\e[5m"
+# define BLK "\e[1;30m"
+# define RED "\e[1;31m"
+# define GRN "\e[1;32m"
+# define YLW "\e[1;33m"
+# define BLU "\e[1;34m"
+# define PRP "\e[1;35m"
+# define CYN "\e[1;36m"
+# define WHT "\e[1;37m"
+# define CLR "\e[0;39m"
+
+# ifdef DEBUG
+#	define MSG(str) std::cout << str << std::endl;
+# else
+#	define MSG(str)
+# endif
+
+class Bureaucrat{
+	private:
+		const std::string	_name;
+		int					_grade;
+		void	sanitizeGrade(int grade);
+	
+	public:
+		Bureaucrat();
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &src);
+		Bureaucrat &operator=(const Bureaucrat &src);
+		Bureaucrat(const std::string name, int grade);
+		const std::string &getName() const;
+		int getGrade() const;
+		void incGrade();
+		void decGrade();
+		class GradeTooHighException : public std::exception{
+			public:
+				virtual const char *what()const throw();
+		};
+		class GradeTooLowException : public std::exception{
+			public:
+				virtual const char *what()const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj);
---
+++ cpp05/ex01/src/Bureaucrat.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 19:08:28 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/07/01 19:10:46 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("defaulterino"), _grade(150){
+	MSG(GRN "Bureacrat default constructor called" RST);
+}
+
+Bureaucrat::~Bureaucrat(){
+	MSG(YLW "Bureaucrat default destructor called" RST);
+}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name){
+	sanitizeGrade(grade);
+	MSG(GRN "Bureaucrat parameter constructor called" RST);
+	this->_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src) : _name(src._name), _grade(src._grade){
+	MSG("Bureaucrat copy constructor called");
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src){
+	if (this != &src)
+		this->_grade = src._grade;
+	MSG("Bureaucrat assignment operator called");
+	return *this;
+}
+
+const std::string &Bureaucrat::getName() const {
+	MSG("getName function called");
+	return _name;
+}
+
+int Bureaucrat::getGrade() const {
+	MSG("getName function called");
+	return _grade;
+}
+
+void	Bureaucrat::incGrade(){
+	MSG("incGrade function called");
+	sanitizeGrade(_grade - 1);
+	_grade--;
+}
+
+void	Bureaucrat::decGrade(){
+	MSG("decGrade function called");
+	sanitizeGrade(_grade + 1);
+	_grade++;
+}
+
+void	Bureaucrat::sanitizeGrade(int grade){
+	MSG(YLW BLN"Grade sanitizer called" RST);
+	if (grade < MAX_GRADE)
+		throw GradeTooHighException();
+	else if (grade > MIN_GRADE)
+		throw GradeTooLowException();
+}
+
+const char	*Bureaucrat::GradeTooHighException::what() const throw(){
+	return (RED BLN "That grade is too damn high!" RST);
+}
+
+const char	*Bureaucrat::GradeTooLowException::what() const throw(){
+	return (PRP BLN "That grade is too damn low!" RST);
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj){
+	out << obj.getName() << ", bureaucrat grade " << obj.getGrade() << ".";
+	return out;
+}
