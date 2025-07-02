bd7ba23b80dfcc32a450eb627a5aeef058d8b1ae
Rapcampo <raphael.campospt@gmail.com>
2025-07-02 14:14:57 +0100

    added test cases for forms

--- cpp05/ex01/src/main.cpp
+++ cpp05/ex01/src/main.cpp
@@ -11,9 +11,9 @@
 /* ************************************************************************** */
 
 #include "../includes/Bureaucrat.hpp"
-#include <exception>
+#include "../includes/Form.hpp"
 
-static void	testSuite(){
+/*static void	testSuiteBur(){
 
 	std::cout << YLW "\nTest 1: incrementing too much\n" RST << std::endl;
 	try{
@@ -65,8 +65,85 @@ static void	testSuite(){
 	} catch(std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
+}*/
+
+static void testSuiteForm(){
+
+	std::cout << YLW "\nTest 1: Form grade too high\n" RST << std::endl;
+	try{
+		Form high("invalidHigh", 0, 10);
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << YLW"\nTest 2: Form grade too high\n" RST << std::endl;
+	try{
+		Form low("invalidLow", 151, 10);
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << GRN"\nTest 3: Valid Form\n" RST << std::endl;
+	try{
+		Form valid("valid", 20, 50);
+		std::cout << valid << std::endl;
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << BLU"\nTest 4: Form sign Bur grade lower than required\n" RST << std::endl;
+	try{
+		Bureaucrat lowb("nobody", 120);
+		Form _2b1a("2b1a", 20, 50);
+		std::cout << lowb << std::endl;
+		std::cout << _2b1a << std::endl;
+		_2b1a.beSigned(lowb);
+		std::cout << _2b1a << std::endl;
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << BLU"\nTest 5: Form sign Bur grade equal than required\n" RST << std::endl;
+	try{
+		Bureaucrat hib("somebody", 20);
+		Form _2b1b("2b1b", 20, 50);
+		std::cout << hib<< std::endl;
+		std::cout << _2b1b << std::endl;
+		_2b1b.beSigned(hib);
+		std::cout << _2b1b << std::endl;
+		_2b1b.beSigned(hib);
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << BLU"\nTest 6: Form sign Bur grade higher than required\n" RST << std::endl;
+	try{
+		Bureaucrat h2b("vicepres", 2);
+		Form _2b1c("2b1c", 10, 2);
+		std::cout << h2b<< std::endl;
+		std::cout << _2b1c << std::endl;
+		_2b1c.beSigned(h2b);
+		std::cout << _2b1c << std::endl;
+		_2b1c.beSigned(h2b);
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << BLU"\nTest 6: Form signed more than once\n" RST << std::endl;
+	try{
+		Bureaucrat h2c("vicepres", 2);
+		Form _2b1d("2b1a", 10, 2);
+		std::cout << h2c<< std::endl;
+		std::cout << _2b1d << std::endl;
+		_2b1d.beSigned(h2c);
+		std::cout << _2b1d << std::endl;
+		_2b1d.beSigned(h2c);
+	} catch(std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
 }
 
 int main(){
-	testSuite();
+//	testSuiteBur();
+	testSuiteForm();
 }
