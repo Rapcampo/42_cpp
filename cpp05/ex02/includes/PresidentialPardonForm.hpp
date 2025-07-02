080a6c17e877cfac4064e67e80044806eeb60b1e
Rapcampo <raphael.campospt@gmail.com>
2025-07-02 15:48:13 +0100

    ex02 base headers done

---
+++ cpp05/ex02/includes/PresidentialPardonForm.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 15:46:11 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/07/02 15:47:24 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+	private:
+		const std::string _target;
+
+	public:
+		PresidentialPardonForm();
+		~PresidentialPardonForm();
+		PresidentialPardonForm(const std::string &target);
+		PresidentialPardonForm(const PresidentialPardonForm &src);
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &src);
+
+		virtual void	execute(const Bureaucrat & executor) const;
+		const std::string &getTarget() const;
+
+		class FormNotSignedException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &out, const PresidentialPardonForm &form);
---
+++ cpp05/ex02/includes/RobotomyRequestForm.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 15:43:30 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/07/02 15:45:41 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm {
+	private:
+		const std::string _target;
+
+	public:
+		RobotomyRequestForm();
+		~RobotomyRequestForm();
+		RobotomyRequestForm(const std::string &target);
+		RobotomyRequestForm(const RobotomyRequestForm &src);
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &src);
+
+		virtual void	execute(const Bureaucrat & executor) const;
+		const std::string &getTarget() const;
+
+		class FormNotSignedException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &out, const RobotomyRequestForm &form);
