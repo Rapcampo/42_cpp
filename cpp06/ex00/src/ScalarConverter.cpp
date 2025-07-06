a8915059d6e1d2e047f666e0bc6637e8a12978a6
Rapcampo <raphael.campospt@gmail.com>
2025-07-06 20:54:02 +0100

    seems to be turning out interesting

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -23,11 +23,77 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &src){
 	return (*this);
 }
 
+typedef bool (*funcs[5])(const std::string &lt);
+
+static bool isPseudo(const std::string &lt);
+static bool isChar(const std::string &lt);
+static bool isInt(const std::string &lt);
+static bool isFloat(const std::string &lt);
+static bool isDouble(const std::string &lt);
+static void checkString(const std::string &lt);
+
+static void checkString(const std::string &lt){
+	int i = -i;
+	while (std::isspace(lt[++i]));
+	if (lt.find_first_not_of(VALID, i) != lt.npos)
+		throw std::exception();
+	if (lt.find_first_of('.') != lt.find_last_of('.') ||
+			lt.find_first_of('f') != lt.find_last_of('f') ||
+			lt.find_last_of('-') != lt.find_last_of('-') ||
+			lt.find_last_of('+') != lt.find_last_of('+'))
+		throw std::exception();
+}
+
+static bool isPseudo(const std::string &lt){
+	return (lt == "-inf" || lt == "+inf" || lt == "+inff" ||
+			lt == "-inff" || lt == "nan" || lt == "nanf");
+}
+
 void	ScalarConverter::convert(const std::string &lt){
-	toChar(lt, static_cast<char>(std::atoi(lt.c_str())));
-	toInt(lt, static_cast<int>(std::atoi(lt.c_str())));
-	toFloat(lt, static_cast<float>(std::strtof(lt.c_str(), 0)));
-	toChar(lt, static_cast<double>(std::strtod(lt.c_str(), 0)));
+	bool foundType = false;
+	int typePos = -1;
+	funcs f = {isChar, isInt, isFloat, isDouble, NULL};
+	if (isPseudo(lt)){
+		toPseudo(lt);
+		return;
+	}
+	try{
+		checkString(lt);
+	}catch(...){
+		std::cout << "this is not a valid type :(" << std::endl;
+		return;
+	}
+	while (foundType != true && ++typePos < 4)
+		foundType = f[typePos](lt);
+	switch (typePos){
+		case 0:
+			toChar(lt, static_cast<char>(std::atoi(lt.c_str())));
+			break;
+		case 1:
+			toInt(lt, static_cast<int>(std::atoi(lt.c_str())));
+			break;
+		case 2:
+			toFloat(lt, static_cast<float>(std::strtof(lt.c_str(), 0)));
+			break;
+		case 3:
+			toDouble(lt, static_cast<double>(std::strtod(lt.c_str(), 0)));
+			break;
+		case 4:
+			std::cout << "Cannot Assert type to convert" << std::endl;
+			break;
+	}
+}
+
+void	ScalarConverter::toPseudo(const std::string &lt){
+	std::cout << "char: impossible\n";
+	std::cout << "int: impossible\n";
+	if (lt.find("nan") != lt.npos){
+		std::cout << "float: nanf\n";
+		std::cout << "double: nan" << std::endl;
+	}else{
+		std::cout << "float: " << lt.find_first_of("+-") << "inff\n";
+		std::cout << "float: " << lt.find_first_of("+-") << "inf" << std::endl;
+	}
 }
 
 void	ScalarConverter::toChar(const std::string &lt, char c){
