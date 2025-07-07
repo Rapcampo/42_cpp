c948941fe3cfd4c57aec8244e5a527c53fcc00cc
Rapcampo <raphael.campospt@gmail.com>
2025-07-07 14:55:16 +0100

    what a silly mess

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -22,3 +22,9 @@ void	iter(T *array, size_t len, void(*f)(T&)){
 		(*f)(array[i]);
 }
 
+template <class T>
+
+void	iter(T *array, size_t len, void(*f)(const T&)){
+	for (size_t i = 0; i < len; i++)
+		(*f)(array[i]);
+}
