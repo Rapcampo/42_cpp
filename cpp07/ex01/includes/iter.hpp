630af85d7ccac53dd2521ee5d455ec162d1e1841
Raphael Campos Vieira <rapcampo@c1r2s2.42porto.com>
2025-07-12 14:32:47 +0100

    minor changes

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -14,14 +14,14 @@
 
 #include <iostream>
 
-template <class T>
+template <typename T>
 
 void	iter(T *array, size_t len, void(*f)(T&)){
 	for (size_t i = 0; i < len; i++)
 		(*f)(array[i]);
 }
 
-template <class T>
+template <typename T>
 
 void	iter(T *array, size_t len, void(*f)(const T&)){
 	for (size_t i = 0; i < len; i++)
