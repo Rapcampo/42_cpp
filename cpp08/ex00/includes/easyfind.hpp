c676f3a105a90d68ac15a337f27cc35f27f6b613
Raphael Campos Vieira <rapcampo@c2r4s11.42porto.com>
2025-08-16 19:28:16 +0100

    starting

---
+++ cpp08/ex00/includes/easyfind.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 17:52:24 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/08/16 18:31:34 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <algorithm>
+#include <exception>
+
+template <typename T>
+
+typename T::iterator	easyfind(T& container, int nb)
+{
+	typename T::iterator res;
+
+	res = (std::find(container.begin(), container.end(), nb));
+		if (res == container.end())
+			throw "result not found";
+	return res;
+}
