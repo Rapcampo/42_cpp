775119a16f65bad7cb3320d49eb4610f87a52ce6
Rapcampo <raphael.campospt@gmail.com>
2025-09-19 21:17:59 +0100

    chose containers for ex02

---
+++ cpp09/ex00/includes/PmergeMe.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/19 21:16:10 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/09/19 21:16:56 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <vector>
+#include <deque>
+
+//both vector and deque offer random read
+//
+
+class PmergeMe{
+
+};
