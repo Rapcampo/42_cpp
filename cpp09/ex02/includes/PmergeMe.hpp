3295444f3ba1425e6ea29f7438ec2df06124c770
Rapcampo <raphael.campospt@gmail.com>
2025-11-12 19:12:19 +0000

    started ex02

---
+++ cpp09/ex02/includes/PmergeMe.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 17:50:00 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/11/12 19:11:34 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <list>
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <algorithm>
+#include <iterator>
+
+//NOTE: merge insertion sort works using divide and conquer, binary search trees
+//also uses sorted pairs, check if need std::utility pair(t n, t n);
+//using list and vector are useful containers to allow showing sequential and random
+//memory
+//
+//positive integer sequence "no negative allowed"
+//"recommended avoiding generic templates"
+//"Before: unsorted n seq \n after: sorted n seq \n Time to process a range of n elements with (container) : " 
+//^ required format
+//error management for duplicates is of own discretion
+
+class PmergeMe{
+
+
+};
