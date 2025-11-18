5fe870ab82ceb4fd59df99120003b9c23022238d
Rapcampo <raphael.campospt@gmail.com>
2025-11-18 21:03:17 +0000

    starting new experience tomorrow

---
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 20:56:58 by rapcampo          #+#    #+#             */
+/*   Updated: 2025/11/18 20:58:49 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/PmergeMe.hpp"
+
+#define A 0
+#define B 1
+#define ODD -1
+
+int	min(int a, int b) {return a < b ? a : b;}
+int	max(int a, int b) {return a > b ? a : b;}
