d6911140ec7b85473fe0c6282dfaffb3ce3ffa5e
Rapcampo <raphael.campospt@gmail.com>
2025-11-21 15:24:58 +0000

    ready to dissect and refactor in a more sensible way

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -12,3 +12,41 @@
 
 #include "../includes/PmergeMe.hpp"
 
+template <typename T>
+void print(T &cont){
+	typename T::iterator it;
+
+	for (it = cont.begin(); it != cont.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+}
+
+int main(int ac, char **av){
+	std::vector<int> vec;
+	std::deque<int> dec;
+
+	PmergeMe sort;
+
+	if (ac < 2)
+		return -1;
+	if (!sort.parse(av + 1, vec, dec))
+		return std::cout << "Error\n", 1;
+	std::cout << "Before: ";
+	print(vec);
+	double vecDeltaT = sort.mergeInsertionSort(vec);
+	double decDeltaT = sort.mergeInsertionSort(dec);
+
+	std::cout << "After: ";
+	print(vec);
+
+	std::cout << "Time to process a range of " << vec.size()
+		<< " elements with std::vector : "
+		<< vecDeltaT << " s /"
+		<< vecDeltaT * 1000 << " ms /"
+		<< vecDeltaT * 1000 * 1000 << " us \n";
+	std::cout << "Time to process a range of " << dec.size()
+		<< " elements with std::deque : "
+		<< decDeltaT << " s /"
+		<< decDeltaT * 1000 << " ms /"
+		<< decDeltaT * 1000 * 1000 << " us \n";
+}
